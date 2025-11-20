9c2f07c72a3512c18e392a34abf88e4fe1cdd7cf
YoyzoA <yorgoassal@gmail.com>
2025-11-20 18:38:25 +0200

    Android App Commit

--- zed_mapping_localization_3d.py
+++ zed_mapping_localization_3d.py
@@ -2,7 +2,7 @@ import pyzed.sl as sl
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+#from mpl_toolkits.mplot3d import Axes3D
 import os, time
 
 # ============================================================
